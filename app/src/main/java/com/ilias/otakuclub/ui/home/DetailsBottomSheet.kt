b18883363b7799f051fc843e3bdbd2305ce35251
ilias iosifidis <iosifidis90@gmail.com>
2026-01-27 19:37:46 +0200

    made details bottom sheet a composable

---
+++ app/src/main/java/com/ilias/otakuclub/ui/home/DetailsBottomSheet.kt
@@ -0,0 +1,213 @@
+package com.ilias.otakuclub.ui.home
+
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
+import com.ilias.otakuclub.R
+import com.ilias.otakuclub.domain.repository.AnimeRepository
+import com.ilias.otakuclub.ui.details.DetailsViewModel
+import com.ilias.otakuclub.ui.details.DetailsViewModelFactory
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsBottomSheet(
+    show: Boolean,
+    selectedAnimeId: Int?,
+    repo: AnimeRepository,
+    onDismiss: () -> Unit,
+) {
+    if (!show) return
+
+    val detailsViewModel: DetailsViewModel =
+        viewModel(factory = DetailsViewModelFactory(repo = repo))
+
+    val detailsUiState by detailsViewModel.uiState.collectAsState()
+    val context = LocalContext.current // for trailer link at Details
+
+    LaunchedEffect(selectedAnimeId) {
+        selectedAnimeId?.let { detailsViewModel.loadAnimeDetails(it) }
+    }
+
+    ModalBottomSheet(
+        onDismissRequest = { onDismiss() }
+    ) {
+        when {
+            // waiting indicator
+            detailsUiState.isLoading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+            // in case of error
+            detailsUiState.errorMessage != null -> {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(detailsUiState.errorMessage!!)
+                    Text(text = "Retry")
+                }
+            }
+            // all went well, make the Bottom Sheet Modal appear
+            detailsUiState.animeDetails != null -> {
+                // BOTTOM SHEET MODAL INTERFACE
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    // title
+                    Text(
+                        text = detailsUiState.animeDetails!!.title,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = detailsUiState.animeDetails!!.titleJap.toString(),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    // image
+                    AsyncImage(
+                        contentScale = ContentScale.FillWidth,
+                        model = detailsUiState.animeDetails!!.imageUrlLarge,
+                        contentDescription = detailsUiState.animeDetails!!.title,
+                        onError = { println(detailsUiState.errorMessage) },
+                        modifier = Modifier.padding(10.dp)
+                    )
+                    // score
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(
+                                    SpanStyle(
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color(0xFF59CC5D)
+                                    )
+                                )
+                                { append("Score: ") }
+                                append("${detailsUiState.animeDetails!!.score}")
+                            },
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+
+                        Icon(
+                            painterResource(R.drawable.outline_star_rate_24),
+                            contentDescription = "star",
+                            tint = Color.Yellow
+                        )
+                    }
+                    // genre
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color(0xFFE91E63)
+                                )
+                            )
+                            { append("Genre: ") }
+                            append("${detailsUiState.animeDetails!!.genres}")
+                        },
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    // aired from-to
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color(0xFF00B0FF)
+                                )
+                            ) { append("Aired  ") }
+                            append("from: ${detailsUiState.animeDetails!!.airedFrom}, to: ${detailsUiState.animeDetails!!.airedTo}")
+                        }
+                    )
+                    // number of episodes
+                    Text(text = "Number of episodes:${detailsUiState.animeDetails!!.episodes}")
+                    // duration
+                    Text(text = "Duration: ${detailsUiState.animeDetails!!.duration}")
+                    Spacer(Modifier.height(10.dp))
+                    // synopsis
+                    Text(
+                        text = "Synopsis",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    HorizontalDivider(Modifier.padding(5.dp))
+                    Text(text = "${detailsUiState.animeDetails!!.synopsis}")
+                    Spacer(Modifier.height(10.dp))
+                    // background
+                    Text(
+                        text = "Background",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    HorizontalDivider(Modifier.padding(5.dp))
+                    Text(text = "${detailsUiState.animeDetails!!.background}")
+                    // trailer
+                    Text(text = "Trailer")
+                    HorizontalDivider(Modifier.padding(5.dp))
+                    TextButton(
+                        onClick = {
+                            detailsUiState.animeDetails!!.trailer?.let {
+                                val intent = Intent(Intent.ACTION_VIEW, it.toUri())
+                                context.startActivity(intent)
+                            }
+                        },
+                        enabled = !detailsUiState.animeDetails!!.trailer.isNullOrBlank()
+                    ) {
+                        Text(text = "Watch Trailer")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
--- app/src/main/java/com/ilias/otakuclub/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ilias/otakuclub/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.ilias.otakuclub.ui.home
 
-import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -8,30 +7,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -47,22 +38,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.net.toUri
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import com.ilias.otakuclub.R
 import com.ilias.otakuclub.data.repository.AnimeRepositoryImpl
-import com.ilias.otakuclub.ui.details.DetailsViewModel
-import com.ilias.otakuclub.ui.details.DetailsViewModelFactory
 import com.ilias.otakuclub.ui.search.SearchViewModel
 import kotlinx.coroutines.launch
 
@@ -83,7 +64,6 @@ fun HomeScreen(
     val gridState = rememberLazyGridState() // for pagination
     val showScrollToTop by remember { derivedStateOf { gridState.firstVisibleItemIndex > 0 } } // for the scroll-up arrow
     val scope = rememberCoroutineScope() // for infinite scroll
-    val context = LocalContext.current // for trailer link at Details
     var selectedAnimeId by remember { mutableStateOf<Int?>(null) }
     var showBottomSheet by remember { mutableStateOf(false) }// for clickable
 
@@ -223,162 +203,12 @@ fun HomeScreen(
                 }
 
                 // OPEN THE ANIME-DETAILS MODAL
-                if (showBottomSheet) {
-
-                    val detailsViewModel: DetailsViewModel =
-                        viewModel(factory = DetailsViewModelFactory(repo = repo))
-                    val detailsUiState by detailsViewModel.uiState.collectAsState()
-
-                    LaunchedEffect(selectedAnimeId) {
-                        detailsViewModel.loadAnimeDetails(selectedAnimeId!!)
-                    }
-
-                    ModalBottomSheet(
-                        onDismissRequest = { showBottomSheet = false }
-                    ) {
-                        when {
-                            // waiting indicator
-                            detailsUiState.isLoading -> {
-                                Box(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(32.dp),
-                                    contentAlignment = Alignment.Center
-                                ) {
-                                    CircularProgressIndicator()
-                                }
-                            }
-                            // in case of error
-                            detailsUiState.errorMessage != null -> {
-                                Column(
-                                    modifier = Modifier.padding(16.dp),
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.SpaceEvenly
-                                ) {
-                                    Text(detailsUiState.errorMessage!!)
-                                    Text(text = "Retry")
-                                }
-                            }
-                            // all went well, make the Bottom Sheet Modal appear
-                            detailsUiState.animeDetails != null -> {
-                                // BOTTOM SHEET MODAL INTERFACE
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(16.dp)
-                                        .verticalScroll(rememberScrollState())
-                                ) {
-                                    // title
-                                    Text(
-                                        text = detailsUiState.animeDetails!!.title,
-                                        style = MaterialTheme.typography.titleLarge
-                                    )
-                                    Text(
-                                        text = detailsUiState.animeDetails!!.titleJap.toString(),
-                                        style = MaterialTheme.typography.titleLarge
-                                    )
-                                    // image
-                                    AsyncImage(
-                                        contentScale = ContentScale.FillWidth,
-                                        model = detailsUiState.animeDetails!!.imageUrlLarge,
-                                        contentDescription = detailsUiState.animeDetails!!.title,
-                                        onError = { println(detailsUiState.errorMessage) },
-                                        modifier = Modifier.padding(10.dp)
-                                    )
-                                    // score
-                                    Row(
-                                        horizontalArrangement = Arrangement.Center,
-                                        modifier = Modifier.fillMaxWidth()
-                                    ) {
-                                        Text(
-                                            text = buildAnnotatedString {
-                                                withStyle(
-                                                    SpanStyle(
-                                                        fontSize = 18.sp,
-                                                        fontWeight = FontWeight.Bold,
-                                                        color = Color(0xFF59CC5D)
-                                                    )
-                                                )
-                                                { append("Score: ") }
-                                                append("${detailsUiState.animeDetails!!.score}")
-                                            },
-                                            style = MaterialTheme.typography.bodyMedium
-                                        )
-
-                                        Icon(
-                                            painterResource(R.drawable.outline_star_rate_24),
-                                            contentDescription = "star",
-                                            tint = Color.Yellow
-                                        )
-                                    }
-                                    // genre
-                                    Text(
-                                        text = buildAnnotatedString {
-                                            withStyle(
-                                                SpanStyle(
-                                                    fontSize = 18.sp,
-                                                    fontWeight = FontWeight.Bold,
-                                                    color = Color(0xFFE91E63)
-                                                )
-                                            )
-                                            { append("Genre: ") }
-                                            append("${detailsUiState.animeDetails!!.genres}")
-                                        },
-                                        style = MaterialTheme.typography.bodyMedium
-                                    )
-                                    // aired from-to
-                                    Text(
-                                        text = buildAnnotatedString {
-                                            withStyle(
-                                                SpanStyle(
-                                                    fontSize = 18.sp,
-                                                    fontWeight = FontWeight.Bold,
-                                                    color = Color(0xFF00B0FF)
-                                                )
-                                            ){append("Aired  ")}
-                                            append("from: ${detailsUiState.animeDetails!!.airedFrom}, to: ${detailsUiState.animeDetails!!.airedTo}")
-                                        }
-                                    )
-                                    // number of episodes
-                                    Text(text = "Number of episodes:${detailsUiState.animeDetails!!.episodes}")
-                                    // duration
-                                    Text(text = "Duration: ${detailsUiState.animeDetails!!.duration}")
-                                    Spacer(Modifier.height(10.dp))
-                                    // synopsis
-                                    Text(
-                                        text = "Synopsis",
-                                        style = MaterialTheme.typography.titleLarge
-                                    )
-                                    HorizontalDivider(Modifier.padding(5.dp))
-                                    Text(text = "${detailsUiState.animeDetails!!.synopsis}")
-                                    Spacer(Modifier.height(10.dp))
-                                    // background
-                                    Text(
-                                        text = "Background",
-                                        style = MaterialTheme.typography.titleLarge
-                                    )
-                                    HorizontalDivider(Modifier.padding(5.dp))
-                                    Text(text = "${detailsUiState.animeDetails!!.background}")
-                                    // trailer
-                                    Text(text = "Trailer")
-                                    HorizontalDivider(Modifier.padding(5.dp))
-                                    TextButton(
-                                        onClick = {
-                                            detailsUiState.animeDetails!!.trailer?.let {
-                                                val intent = Intent(Intent.ACTION_VIEW, it.toUri())
-                                                context.startActivity(intent)
-                                            }
-                                        },
-                                        enabled = !detailsUiState.animeDetails!!.trailer.isNullOrBlank()
-                                    ) {
-                                        Text(text = "Watch Trailer")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                DetailsBottomSheet(
+                    show = showBottomSheet,
+                    selectedAnimeId = selectedAnimeId,
+                    repo = repo,
+                    onDismiss = { showBottomSheet = false }
+                )
             }
         }
     }
